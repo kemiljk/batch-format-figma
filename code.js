7f5d0d7684a9bbd8cb3a7d6ef60eb2ef50b2fa91
Karl Koch <karl@kejk.tech>
2021-04-08 13:15:16 +0100

    init

--- code.js
+++ code.js
@@ -7,45 +7,87 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-figma.showUI(__html__, { width: 300, height: 105 });
+figma.showUI(__html__, { width: 300, height: 300 });
 const nodeTypes = ["RECTANGLE", "ELLIPSE", "POLYGON", "STAR"];
-// const layers = figma.currentPage.findAll((node) =>
-//   nodeTypes.includes(node.type)
-// );
-// figma.currentPage.selection = layers;
 const { selection } = figma.currentPage;
+function clone(val) {
+    return JSON.parse(JSON.stringify(val));
+}
 figma.ui.onmessage = (msg) => {
     if (msg.type === "set-to-fill") {
+        const images = figma.currentPage.selection;
         function setToFill() {
             return __awaiter(this, void 0, void 0, function* () {
-                const nodes = [];
                 figma.root.children.flatMap((pageNode) => pageNode.selection.forEach((node) => __awaiter(this, void 0, void 0, function* () {
                     if (nodeTypes.includes(node.type)) {
-                        node.fills.scaleMode = "FILL";
+                        node.fills = node.fills.map((p) => p.type === "IMAGE" ? Object.assign(Object.assign({}, p), { scaleMode: "FILL" }) : p);
+                        node.resize(msg.widthCount, msg.heightCount);
                     }
                 })));
-                // shape.resize(900, 400)
-                figma.currentPage.selection = nodes;
-                figma.viewport.scrollAndZoomIntoView(nodes);
             });
         }
         setToFill();
+        figma.notify(`Set ${images.length} images to FILL`);
     }
     if (msg.type === "set-to-fit") {
+        const images = figma.currentPage.selection;
         function setToFit() {
             return __awaiter(this, void 0, void 0, function* () {
-                const nodes = [];
                 figma.root.children.flatMap((pageNode) => pageNode.selection.forEach((node) => __awaiter(this, void 0, void 0, function* () {
                     if (nodeTypes.includes(node.type)) {
-                        node.fills.scaleMode = "FIT";
+                        node.fills = node.fills.map((p) => p.type === "IMAGE" ? Object.assign(Object.assign({}, p), { scaleMode: "FIT" }) : p);
+                        node.resize(msg.widthCount, msg.heightCount);
                     }
                 })));
-                // shape.resize(900, 400)
-                figma.currentPage.selection = nodes;
-                figma.viewport.scrollAndZoomIntoView(nodes);
             });
         }
         setToFit();
+        figma.notify(`Set ${images.length} images to FIT`);
     }
-    figma.closePlugin();
+    if (msg.type === "set-to-crop") {
+        const images = figma.currentPage.selection;
+        function setToCrop() {
+            return __awaiter(this, void 0, void 0, function* () {
+                figma.root.children.flatMap((pageNode) => pageNode.selection.forEach((node) => __awaiter(this, void 0, void 0, function* () {
+                    if (nodeTypes.includes(node.type)) {
+                        node.fills = node.fills.map((p) => p.type === "IMAGE" ? Object.assign(Object.assign({}, p), { scaleMode: "CROP" }) : p);
+                        node.resize(msg.widthCount, msg.heightCount);
+                    }
+                })));
+            });
+        }
+        setToCrop();
+        figma.notify(`Set ${images.length} images to CROP`);
+    }
+    if (msg.type === "set-to-tile") {
+        const images = figma.currentPage.selection;
+        function setToTile() {
+            return __awaiter(this, void 0, void 0, function* () {
+                figma.root.children.flatMap((pageNode) => pageNode.selection.forEach((node) => __awaiter(this, void 0, void 0, function* () {
+                    if (nodeTypes.includes(node.type)) {
+                        node.fills = node.fills.map((p) => p.type === "IMAGE" ? Object.assign(Object.assign({}, p), { scaleMode: "TILE" }) : p);
+                        node.resize(msg.widthCount, msg.heightCount);
+                    }
+                })));
+            });
+        }
+        setToTile();
+        figma.notify(`Set ${images.length} images to TILE`);
+    }
+    if (msg.type === "remove-fill-layer") {
+        function removeFillLayer() {
+            return __awaiter(this, void 0, void 0, function* () {
+                figma.root.children.flatMap((pageNode) => pageNode.selection.forEach((node) => __awaiter(this, void 0, void 0, function* () {
+                    const fills = clone(node.fills);
+                    if (fills.length >= 2) {
+                        if (node.type !== "IMAGE") {
+                            fills.splice(0, 1);
+                            node.fills = fills;
+                        }
+                    }
+                })));
+            });
+        }
+        removeFillLayer();
+    }
 };
--- code.ts
+++ code.ts
@@ -1,45 +1,96 @@
-figma.showUI(__html__, { width: 300, height: 105 });
+figma.showUI(__html__, { width: 300, height: 300 });
 const nodeTypes = ["RECTANGLE", "ELLIPSE", "POLYGON", "STAR"];
 
-// const layers = figma.currentPage.findAll((node) =>
-//   nodeTypes.includes(node.type)
-// );
-// figma.currentPage.selection = layers;
 const { selection } = figma.currentPage;
+
+function clone(val) {
+  return JSON.parse(JSON.stringify(val));
+}
 
 figma.ui.onmessage = (msg) => {
   if (msg.type === "set-to-fill") {
+    const images = figma.currentPage.selection;
     async function setToFill() {
-      const nodes: SceneNode[] = [];
       figma.root.children.flatMap((pageNode) =>
         pageNode.selection.forEach(async (node) => {
           if (nodeTypes.includes(node.type)) {
-            node.fills.scaleMode = "FILL";
+            node.fills = node.fills.map((p) =>
+              p.type === "IMAGE" ? { ...p, scaleMode: "FILL" } : p
+            );
+            node.resize(msg.widthCount, msg.heightCount);
           }
         })
       );
-      // shape.resize(900, 400)
-      figma.currentPage.selection = nodes;
-      figma.viewport.scrollAndZoomIntoView(nodes);
     }
     setToFill();
+    figma.notify(`Set ${images.length} images to FILL`);
   }
   if (msg.type === "set-to-fit") {
+    const images = figma.currentPage.selection;
     async function setToFit() {
-      const nodes: SceneNode[] = [];
       figma.root.children.flatMap((pageNode) =>
         pageNode.selection.forEach(async (node) => {
           if (nodeTypes.includes(node.type)) {
-            node.fills.scaleMode = "FIT";
+            node.fills = node.fills.map((p) =>
+              p.type === "IMAGE" ? { ...p, scaleMode: "FIT" } : p
+            );
+            node.resize(msg.widthCount, msg.heightCount);
           }
         })
       );
-      // shape.resize(900, 400)
-      figma.currentPage.selection = nodes;
-      figma.viewport.scrollAndZoomIntoView(nodes);
     }
     setToFit();
+    figma.notify(`Set ${images.length} images to FIT`);
+  }
+  if (msg.type === "set-to-crop") {
+    const images = figma.currentPage.selection;
+    async function setToCrop() {
+      figma.root.children.flatMap((pageNode) =>
+        pageNode.selection.forEach(async (node) => {
+          if (nodeTypes.includes(node.type)) {
+            node.fills = node.fills.map((p) =>
+              p.type === "IMAGE" ? { ...p, scaleMode: "CROP" } : p
+            );
+            node.resize(msg.widthCount, msg.heightCount);
+          }
+        })
+      );
+    }
+    setToCrop();
+    figma.notify(`Set ${images.length} images to CROP`);
+  }
+  if (msg.type === "set-to-tile") {
+    const images = figma.currentPage.selection;
+    async function setToTile() {
+      figma.root.children.flatMap((pageNode) =>
+        pageNode.selection.forEach(async (node) => {
+          if (nodeTypes.includes(node.type)) {
+            node.fills = node.fills.map((p) =>
+              p.type === "IMAGE" ? { ...p, scaleMode: "TILE" } : p
+            );
+            node.resize(msg.widthCount, msg.heightCount);
+          }
+        })
+      );
+    }
+    setToTile();
+    figma.notify(`Set ${images.length} images to TILE`);
   }
 
-  figma.closePlugin();
+  if (msg.type === "remove-fill-layer") {
+    async function removeFillLayer() {
+      figma.root.children.flatMap((pageNode) =>
+        pageNode.selection.forEach(async (node) => {
+          const fills = clone(node.fills);
+          if (fills.length >= 2) {
+            if (node.type !== "IMAGE") {
+              fills.splice(0, 1);
+              node.fills = fills;
+            }
+          }
+        })
+      );
+    }
+    removeFillLayer();
+  }
 };
